15aec4be2fb5401f9f6f05e2cad8fa75f9248aa5
sara-qayyum <i200556@nu.edu.pk>
2021-12-02 15:10:21 +0500

    my first commit

---
+++ src/First_File.java
@@ -0,0 +1,8 @@
+public class First_File {
+    public static void main(String[] args) {
+        String name = "Momin";
+        String rollno = "18i-0574";
+        System.out.println("Name: " + name);
+        System.out.println("RollNo: " + rollno);
+    }
+}
