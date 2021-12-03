41456b44264ba092864899be619b923e2bb9eaa7
sara-qayyum <95412560+sara-qayyum@users.noreply.github.com>
2021-12-03 17:22:50 +0500

    Update First_File.java

--- src/First_File.java
+++ src/First_File.java
@@ -1,6 +1,5 @@
 public class First_File {
     public static void main(String[] args) {
-        String name = "Sara";
         String rollno = "20I-0556";
         System.out.println("Name: " + name);
         System.out.println("RollNo: " + rollno);
