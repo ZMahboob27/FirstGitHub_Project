58cb66ce641573a01607b8f46866695bb110b9bd
sara-qayyum <95412560+sara-qayyum@users.noreply.github.com>
2021-12-03 17:50:28 +0500

    Update First_File.java

--- src/First_File.java
+++ src/First_File.java
@@ -1,13 +1,8 @@
 public class First_File {
     public static void main(String[] args) {
         String name = "Sara";
-        String name1 = "Hasham ul haq";
-        String groupMemberName = "Muhammad Hasham";
         String rollno = "20I-0556";
         System.out.println("Name: " + name);
-        System.out.println("RollNo: " + rollno);
-      
-        System.out.println("Name: " + name1);
         System.out.println("RollNo: " + rollno);
     }
 }
